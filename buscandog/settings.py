365b0c068f3ad8e2e55e426adf1685344ab83039
mauro <mfara.mf@gmail.com>
2022-06-16 17:27:29 -0300

    Static change

--- buscandog/settings.py
+++ buscandog/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles') #location of the static files for deploying
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media') #location on the file system
 MEDIA_URL = '/media/' #
