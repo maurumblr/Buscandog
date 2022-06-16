04730a9196dbe395b2e0491f3330cd93a731dcc2
mauro <mfara.mf@gmail.com>
2022-06-16 19:16:41 -0300

    asds

--- buscandog/settings.py
+++ buscandog/settings.py
@@ -147,9 +147,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = BASE_DIR / 'buscandog/static'
+
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media') #location on the file system
 MEDIA_URL = '/media/' #
